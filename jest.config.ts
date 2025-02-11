1fb8018d1504cd22a35548b63a6b0ea11d9df977
Erenen1 <erencelikk212@gmail.com>
2025-02-11 16:36:41 +0300

    chore(test): Update Jest configuration - Configure test environment - Add coverage settings - Configure test paths - Add test timeouts

---
+++ jest.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  setupFiles: ['<rootDir>/tests/setup.ts'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/types/**/*.ts',
+    '!src/config/**/*.ts',
+    '!src/**/index.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  verbose: true,
+  testTimeout: 10000
+};
+
+export default config; 
